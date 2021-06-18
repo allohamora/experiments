52f11e06eef0f04d3ba0f4e177f446d365f1a089
Allohamora <herman2000@yandex.ru>
2021-06-18 13:19:18 +0300

    Add ecmascript-modules

---
+++ ecmascript-modules/mjs/index.mjs
@@ -0,0 +1,54 @@
+import { sum } from './sum.mjs';
+import { strict as assert } from 'assert';
+import { performance } from 'perf_hooks';
+import { promises as fps } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// .mjs doesn't have __dirname and __filename but have import.meta.url
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const start = performance.now();
+const result = sum(1, 2);
+const expected = 3;
+
+const RESULT_PATH = path.join(__dirname, 'result.json');
+
+const exists = async (path) => {
+  try {
+    await fps.access(path);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+const writeToResult = async (data) => {
+  const isFileExists = await exists(RESULT_PATH);
+  const state = isFileExists ? JSON.parse(await fps.readFile(RESULT_PATH)) : [];
+
+  state.push(data);
+
+  const finalData = JSON.stringify(state, null, 2);
+
+  await fps.writeFile(RESULT_PATH, finalData);
+}
+
+const getMemoryUsedMb = () => {
+  const memoryUsed = process.memoryUsage().heapUsed / 1024 / 1024;
+  const memoryUsedMb = Math.round(memoryUsed * 100) / 100;
+
+  return memoryUsedMb;
+}
+
+const test = () => {
+  assert.deepEqual(result, expected);
+
+  const end = performance.now();
+  const time = end - start;
+  const memory = getMemoryUsedMb();
+
+  return { time, memory }
+}
+
+writeToResult(test());
---
+++ ecmascript-modules/mjs/sum.mjs
@@ -0,0 +1,7 @@
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns number
+ */
+ export const sum = (a, b) => a + b;
