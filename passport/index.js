ae11cf4b1a4ea181c9620a8c36df20cded3a512e
Allohamora <herman2000@yandex.ru>
2021-08-23 13:03:25 +0300

    feat(passport): add passport experiment

---
+++ passport/index.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const express = require('express');
+const passport = require('passport');
+const { strategy } = require('./strategy');
+
+passport.use(strategy);
+
+const port = 3000;
+const indexHtmlPath = path.resolve('index.html');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.post('/login', passport.authenticate('local', { session: false }), (req, res) => {
+  console.log(req.user);
+  res.redirect('/');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(indexHtmlPath);
+});
+
+app.listen(port, () => console.log(`server started on: http://localhost:${port}`));
