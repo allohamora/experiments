ee58b48353d5d1b0bd0760bb2d326a6eff6c4dfe
Allohamora <herman2000@yandex.ru>
2021-09-23 20:38:17 +0300

    feat(monad): add monad experiment

---
+++ monad/index.ts
@@ -0,0 +1,25 @@
+import { strict } from 'assert';
+
+class Monad<V = null> {
+  constructor(private _value: V){}
+
+  public map<T> (func: (value: NonNullable<V>) => T): Monad<T> {
+    const value = this._value;
+    if( value === null ) return this as unknown as Monad<T>;
+
+    return new Monad(func(value as NonNullable<V>));
+  }
+
+  public value(): V | null {
+    return this._value;
+  }
+}
+
+const text = '123';
+
+const notNullable = new Monad({ text })
+  .map(({ text }) => ({ data: text }))
+  .map(({ data }) => data)
+  .value();
+
+strict.deepEqual(notNullable, text);
