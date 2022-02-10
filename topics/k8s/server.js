145ed52547b3c4f9b5aba1e629dbe04dca669b5d
Allohamora <herman2000@yandex.ru>
2022-02-10 16:26:01 +0200

    feat(k8s): add allomora/hello-world:v2
    adds routing, logging, hostname displaying

--- topics/k8s/server.js
+++ topics/k8s/server.js
@@ -1,10 +1,50 @@
 import { createServer } from 'http';
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, HOSTNAME } = process.env;
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.end('hello world!');
-});
+const chain =
+  (...funcs) =>
+  (...args) => {
+    for (const func of funcs) {
+      func(...args);
+    }
+  };
 
-server.listen(PORT, () => console.log(`server started on port: ${PORT}`));
+const logging = (req, res) => {
+  const ip = req.socket.remoteAddress;
+  const userAgent = req.headers['user-agent'];
+  const start = Date.now();
+
+  req.once('close', () => {
+    const finish = Date.now();
+    const requestTime = `${finish - start}ms`;
+
+    console.log({ ip, userAgent, requestTime });
+  });
+};
+
+const routes = {
+  '/': (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('hello world!');
+  },
+};
+
+const routing = (req, res) => {
+  const { url } = req;
+  const route = routes[url];
+
+  if (!route) {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('404');
+    return;
+  }
+
+  route(req, res);
+};
+
+const server = createServer(chain(logging, routing));
+
+server.listen(PORT, () => console.log(`server started on port: ${PORT}, hostname: ${HOSTNAME}`));
