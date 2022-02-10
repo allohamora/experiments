908bbf0a0b26973f3329341461ab8092f352e981
Allohamora <herman2000@yandex.ru>
2022-02-10 20:24:35 +0200

    feat(k8s): add hello-world server v5

--- topics/k8s/server.js
+++ topics/k8s/server.js
@@ -2,15 +2,27 @@ import { createServer } from 'http';
 
 const { PORT = 3000, HOSTNAME } = process.env;
 
-const chain =
-  (...funcs) =>
-  (...args) => {
+const chain = (...funcs) => {
+  return (...args) => {
     for (const func of funcs) {
       func(...args);
     }
   };
+};
 
-const logging = (req, res) => {
+const FIB_DEFAULT_INPUT = 42;
+const fib = (number = FIB_DEFAULT_INPUT) => {
+  if (number === 2) {
+    return 1;
+  }
+  if (number === 1 || number < 1) {
+    return 0;
+  }
+
+  return fib(number - 1) + fib(number - 2);
+};
+
+const logging = ({ req }) => {
   const ip = req.socket.remoteAddress;
   const userAgent = req.headers['user-agent'];
   const start = Date.now();
@@ -23,28 +35,42 @@ const logging = (req, res) => {
   });
 };
 
+const replyFactory = (res) => {
+  return ({ code = 200, type = 'text/plain', message = 'OK' }) => {
+    res.statusCode = code;
+    res.setHeader('Content-Type', type);
+    res.end(message);
+  };
+};
+
 const routes = {
-  '/': (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('hello world!');
+  '/': ({ reply }) => {
+    reply({ code: 200, message: 'hello world!' });
+  },
+  '/fib': ({ reply }) => {
+    reply({ code: 200, message: `result: ${fib()}` });
   },
 };
 
-const routing = (req, res) => {
+const routing = (ctx) => {
+  const { req, reply } = ctx;
   const { url } = req;
   const route = routes[url];
 
   if (!route) {
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('404');
+    reply({ code: 404, message: '404' });
     return;
   }
 
-  route(req, res);
+  route(ctx);
 };
 
-const server = createServer(chain(logging, routing));
+const handler = chain(logging, routing);
+
+const server = createServer((req, res) => {
+  const reply = replyFactory(res);
+
+  handler({ req, res, reply });
+});
 
 server.listen(PORT, () => console.log(`server started on port: ${PORT}, hostname: ${HOSTNAME}`));
