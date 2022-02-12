188195d00219cff7b5ec82b5e24c81ced0c21e73
Allohamora <herman2000@yandex.ru>
2022-02-12 11:17:22 +0200

    feat(compare-objects): add many compare

--- topics/compare-objects/index.js
+++ topics/compare-objects/index.js
@@ -34,11 +34,33 @@ const userCopy = {
   },
 };
 
+const manyCompare = ({ compareWay, values }) => {
+  for (const value of values) {
+    const restValues = values.filter((innerValue) => innerValue !== value);
+
+    for (const anotherValue of restValues) {
+      if (compareWay(value, anotherValue)) {
+        continue;
+      }
+
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const ITEMS_COUNT = 100;
+
 const main = async () => {
   const assert = await performanceTest(() => assertWay(userCopy, user));
   const my = await performanceTest(() => myWay(user, userCopy));
 
-  console.log({ assert, my });
+  const values = Array.from({ length: ITEMS_COUNT }, () => (Math.random() > 0.5 ? user : userCopy));
+  const assertMany = await performanceTest(() => manyCompare({ compareWay: assertWay, values }));
+  const myMany = await performanceTest(() => manyCompare({ compareWay: myWay, values }));
+
+  console.log({ assert, my, assertMany, myMany });
 };
 
 main();
