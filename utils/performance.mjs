bbf74dcaa8bd2bcd51adc616e24b18fd3c2c493a
Allohamora <herman2000@yandex.ru>
2022-02-13 10:01:07 +0200

    feat: improve performance output

--- utils/performance.mjs
+++ utils/performance.mjs
@@ -1,16 +1,19 @@
-export const getUsedMemoryInMb = () => {
-  return Math.round((process.memoryUsage().heapUsed / 1024 / 1024) * 100) / 100;
+const FIXED = 3;
+
+export const getHeapUsedMemoryInMb = () => {
+  const memoryInMb = Math.round((process.memoryUsage().heapUsed / 1024 / 1024) * 100) / 100;
+
+  return `${memoryInMb.toFixed(FIXED)}mb`;
 };
 
 export const performanceTest = async (fn) => {
   const start = performance.now();
-
   const result = await Promise.resolve(fn());
-
   const end = performance.now();
 
-  const time = end - start;
-  const memory = getUsedMemoryInMb();
+  const executionTimeInMs = end - start;
+  const time = `${executionTimeInMs.toFixed(FIXED)}ms`;
+  const memory = getHeapUsedMemoryInMb();
 
   return { time, memory, result };
 };
