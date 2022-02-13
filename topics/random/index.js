70a698ec5a50e099711068149e1b36a4cbe48a35
Allohamora <herman2000@yandex.ru>
2022-02-13 10:41:59 +0200

    feat(random): add random experiment

---
+++ topics/random/index.js
@@ -0,0 +1,22 @@
+import { randomInt } from 'crypto';
+import { manyPerformaneTest, averageNumber } from '../../utils/performance.mjs';
+
+const myRandom = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const cryptoRandom = randomInt;
+
+const MIN = 0;
+const MAX = 10;
+const RUN_COUNT = 100;
+
+const main = async () => {
+  const baseOptions = { runCount: RUN_COUNT, averageResult: averageNumber };
+  const my = await manyPerformaneTest({ ...baseOptions, target: () => myRandom(MIN, MAX) });
+  const crypto = await manyPerformaneTest({ ...baseOptions, target: () => cryptoRandom(MIN, MAX) });
+
+  console.log({ my, crypto });
+};
+
+main();
--- utils/performance.mjs
+++ utils/performance.mjs
@@ -1,19 +1,65 @@
+const MEMORY_MEASURE = 'mb';
+const TIME_MEASURE = 'ms';
 const FIXED = 3;
 
 export const getHeapUsedMemoryInMb = () => {
   const memoryInMb = Math.round((process.memoryUsage().heapUsed / 1024 / 1024) * 100) / 100;
 
-  return `${memoryInMb.toFixed(FIXED)}mb`;
+  return `${memoryInMb.toFixed(FIXED)}${MEMORY_MEASURE}`;
 };
 
-export const performanceTest = async (fn) => {
+export const performanceTest = async (target) => {
   const start = performance.now();
-  const result = await Promise.resolve(fn());
+  const result = await Promise.resolve(target());
   const end = performance.now();
 
   const executionTimeInMs = end - start;
-  const time = `${executionTimeInMs.toFixed(FIXED)}ms`;
+  const time = `${executionTimeInMs.toFixed(FIXED)}${TIME_MEASURE}`;
   const memory = getHeapUsedMemoryInMb();
 
   return { time, memory, result };
 };
+
+export const averageNumber = (numbers) => {
+  const sum = numbers.reduce((state, num) => state + num, 0);
+  const count = numbers.length;
+  const fixedString = (sum / count).toFixed(FIXED);
+
+  return Number(fixedString);
+}
+
+export const averageStringNumber = (stringNumbers) => {
+  const numbers = [];
+
+  for( let stringNumber of stringNumbers ) {
+    numbers.push(parseFloat(stringNumber));
+  }
+
+  return averageNumber(numbers);
+}
+
+export const calculateAverage = ({ results, averageResult }) => {
+  const { timeValues, memoryValues, resultValues } = results.reduce((state, { time, memory, result }) => {
+    state.timeValues.push(time);
+    state.memoryValues.push(memory);
+    state.resultValues.push(result);
+
+    return state;
+  }, { timeValues: [], memoryValues: [], resultValues: [] })
+
+  const time = `${averageStringNumber(timeValues)}${TIME_MEASURE}`;
+  const memory = `${averageStringNumber(memoryValues)}${MEMORY_MEASURE}`;
+  const result = averageResult(resultValues);
+
+  return { time, memory, result };
+}
+
+export const manyPerformaneTest = async ({ target, runCount, averageResult = (results) => results }) => {
+  const results = [];
+
+  for (let i = 0; i < runCount; i++) {
+    results.push(await performanceTest(target));
+  }
+
+  return calculateAverage({ results, averageResult });
+}
