a8c3d96e4506b6e88c1ca6aab2dc6166acd47e8b
Allohamora <herman2000@yandex.ru>
2022-02-14 10:19:15 +0200

    feat(chart.js): add mobile view

--- topics/chart.js/src/main.ts
+++ topics/chart.js/src/main.ts
@@ -8,6 +8,7 @@ import {
   Legend,
   Tooltip,
   Filler,
+  ChartConfiguration,
 } from 'chart.js';
 import './style.css';
 
@@ -27,7 +28,7 @@ app.append(chartBlock);
 const data = [-1, 3, 2, 1, 3, 2].map((num) => num * 100);
 
 const POINT_RADIUS = 2; // chart.js incorrectly expands point with 0 radius
-const HOVER_POINT_RADIUS = 13;
+const HOVER_POINT_RADIUS = 12;
 const LAST_POINT_RADIUS = HOVER_POINT_RADIUS;
 
 const getLastRestPointOptions = <T>({
@@ -42,7 +43,94 @@ const getLastRestPointOptions = <T>({
   return data.slice(0, data.length - 1).map((item, i, arr) => (i === arr.length - 1 ? lastValue : restValues));
 };
 
-new Chart(ctx, {
+const desktopOptions: ChartConfiguration<'line'>['options'] = {
+  responsive: true,
+  elements: {
+    line: {
+      borderWidth: 5,
+      cubicInterpolationMode: 'monotone',
+      fill: 'start',
+      borderColor: 'rgb(75, 192, 192)',
+      backgroundColor: 'rgba(75, 192, 192, .2)',
+    },
+    point: {
+      borderWidth: 2,
+      hoverRadius: HOVER_POINT_RADIUS,
+      hitRadius: 30,
+      backgroundColor: 'rgba(75, 192, 192)',
+      borderColor: 'rgba(75, 192, 192)',
+    },
+  },
+  plugins: {
+    legend: {
+      display: false,
+    },
+    tooltip: {
+      displayColors: false,
+      caretSize: 0,
+      caretPadding: 0,
+      cornerRadius: 0,
+      footerSpacing: 0,
+      footerMarginTop: 0,
+      bodySpacing: 0,
+      titleSpacing: 0,
+      padding: 0,
+      titleColor: 'black',
+      backgroundColor: 'rgba(0, 0, 0, 0)',
+      titleMarginBottom: -6,
+      xAlign: 'center',
+      yAlign: 'bottom',
+      titleFont: {
+        size: 10,
+        weight: 'bold',
+      },
+      callbacks: {
+        title: (items) => items[0].formattedValue,
+        label: () => '',
+      },
+    },
+  },
+  scales: {
+    y: {
+      grid: {
+        display: false,
+        drawBorder: false,
+      },
+      ticks: {
+        display: false,
+      },
+      grace: 5,
+    },
+    x: {
+      grid: {
+        display: false,
+        drawBorder: false,
+      },
+      ticks: {
+        font: {
+          size: 13,
+          weight: 'bold',
+        },
+      },
+    },
+  },
+};
+
+const mobileOptions: ChartConfiguration<'line'>['options'] = {
+  ...desktopOptions,
+  scales: {
+    ...desktopOptions.scales,
+    x: {
+      ...desktopOptions.scales!.x,
+      ticks: {
+        ...desktopOptions.scales!.x?.ticks,
+        display: false,
+      },
+    },
+  },
+};
+
+const chart = new Chart(ctx, {
   type: 'line',
   data: {
     labels: ['16:00', '17:00', '18:00', '19:00', '20:00'],
@@ -54,83 +142,27 @@ new Chart(ctx, {
       },
     ],
   },
-  options: {
-    responsive: true,
-    elements: {
-      line: {
-        borderWidth: 5,
-        cubicInterpolationMode: 'monotone',
-        fill: 'start',
-        borderColor: 'rgb(75, 192, 192)',
-        backgroundColor: 'rgba(75, 192, 192, .2)',
-      },
-      point: {
-        // radius: 0,
-        // borderWidth: 0,
-        // hoverRadius: 0,
-        // hitRadius: 0,
-        borderWidth: 2,
-        hoverRadius: HOVER_POINT_RADIUS,
-        hitRadius: 30,
-        backgroundColor: 'rgba(75, 192, 192)',
-        borderColor: 'rgba(75, 192, 192)',
-      },
-    },
-    plugins: {
-      legend: {
-        display: false,
-      },
-      tooltip: {
-        // enabled: false,
-        displayColors: false,
-        caretSize: 0,
-        caretPadding: 0,
-        cornerRadius: 0,
-        footerSpacing: 0,
-        footerMarginTop: 0,
-        bodySpacing: 0,
-        titleSpacing: 0,
-        padding: 0,
-        titleColor: 'black',
-        backgroundColor: 'rgba(0, 0, 0, 0)',
-        titleMarginBottom: -6,
-        xAlign: 'center',
-        yAlign: 'bottom',
-        titleFont: {
-          size: 10,
-          weight: 'bold',
-        },
-        callbacks: {
-          title: (items) => items[0].formattedValue,
-          label: () => '',
-        },
-      },
-    },
-    scales: {
-      y: {
-        grid: {
-          display: false,
-          drawBorder: false,
-          // tickLength: 0
-        },
-        ticks: {
-          display: false,
-        },
-        grace: 5,
-      },
-      x: {
-        grid: {
-          display: false,
-          drawBorder: false,
-          // tickLength: 0
-        },
-        ticks: {
-          font: {
-            size: 13,
-            weight: 'bold',
-          },
-        },
-      },
-    },
-  },
+  options: desktopOptions,
 });
+
+const toDesktop = (chart: Chart) => {
+  chart.options = desktopOptions;
+};
+
+const toMobile = (chart: Chart) => {
+  chart.options = mobileOptions;
+};
+
+const chartResizeHandler = () => {
+  if (window.matchMedia('(max-width: 550px)').matches) {
+    toMobile(chart);
+  } else {
+    toDesktop(chart);
+  }
+
+  chart.update();
+  chart.resize();
+};
+
+window.addEventListener('resize', chartResizeHandler);
+chartResizeHandler();
