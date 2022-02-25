24b1a4243c1bb306429eb7ab2f64c18b1dab44a0
Allohamora <herman2000@yandex.ru>
2022-02-25 11:06:35 +0200

    feat(fastify): add swagger

--- topics/fastify/src/index.ts
+++ topics/fastify/src/index.ts
@@ -1,15 +1,30 @@
 import fastify from 'fastify';
+import swagger from 'fastify-swagger';
 import { sum } from './math.js';
 
 const PORT = Number(process.env.PORT || 3000);
 
 const app = fastify({ logger: true });
 
+app.register(swagger, {
+  routePrefix: '/api',
+  swagger: {
+    info: {
+      title: 'api',
+      description: 'Testing the Fastify swagger API',
+      version: '0.0.0',
+    },
+    tags: [{ name: 'math' }],
+  },
+  exposeRoute: true,
+});
+
 app.get('/', async () => 'hello world!');
 app.route<{ Querystring: { a: number; b: number } }>({
   url: '/sum',
   method: 'GET',
   schema: {
+    tags: ['math'],
     querystring: {
       type: 'object',
       properties: {
