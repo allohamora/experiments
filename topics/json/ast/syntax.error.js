1bf11ea3a0a9baaf49d6ca116ba88eeabb711a8c
allohamora <herman30698@gmail.com>
2022-03-07 17:04:01 +0200

    feat(json): improve syntax error message

--- topics/json/ast/syntax.error.js
+++ topics/json/ast/syntax.error.js
@@ -1,5 +1,43 @@
 export class SyntaxError extends Error {
+  // # private fields because _ private fields are displayed in console
+  #range;
+  #source;
+
   constructor(range, source) {
-    super(`invalid token on position: ${range.join('-')}. raw: =>'${source.slice(range[0])}'`);
+    super('');
+
+    this.#range = range;
+    this.#source = source;
+
+    this.message = this.createMessage();
+  }
+
+  createMessage() {
+    const rangeDiff = this.#range[1] - this.#range[0];
+
+    if( rangeDiff < 2 ) {
+      return this.createTokenMessage();
+    } else {
+      return this.createRangeMessage();
+    }
+  }
+
+  createTokenMessage() {
+    const index = this.#range[0];
+
+    return `unexpected token on position: ${index}
+'${this.#source}'
+ ${' '.repeat(index)}^
+`
+  }
+
+  createRangeMessage() {
+    const range = this.#range.join('-');
+    const slice = this.#source.slice(this.#range[0], this.#range[1]);
+
+    return `unexpected input on range: ${range}
+${slice}
+${'-'.repeat(slice.length)}
+`;
   }
 }
