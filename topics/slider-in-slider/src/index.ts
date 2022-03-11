c51d387085f68e177bcd0fcab9672da1695c2a1f
allohamora <herman30698@gmail.com>
2022-03-11 11:22:35 +0200

    feat(slider-in-slider): migrate autoplay to swiper listeners

--- topics/slider-in-slider/src/index.ts
+++ topics/slider-in-slider/src/index.ts
@@ -1,65 +1,58 @@
 import 'swiper/css';
 import 'swiper/css/pagination';
 import './index.css';
-import Swiper, { Pagination } from 'swiper';
+import Swiper, { Pagination, Autoplay } from 'swiper';
 
-const mainSwiper = new Swiper('.main-swiper', {
-  modules: [Pagination],
-  direction: 'horizontal',
-  loop: true,
-  spaceBetween: 50,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-});
-
-const nestedSwiper = new Swiper('.nested-swiper', {
-  modules: [Pagination],
-  direction: 'vertical',
-  spaceBetween: 50,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-});
+const NESTED_INDEX = 1;
 
 const nestedSlides = document.querySelectorAll('.nested-swiper .swiper-slide');
 const nestedSlidesCount = nestedSlides.length;
 
-const AUTOPLAY_DELAY = 1000;
-const NESTED_INDEX = 1;
+const nestedSwiper = new Swiper('.nested-swiper', {
+  modules: [Pagination, Autoplay],
+  direction: 'vertical',
+  spaceBetween: 50,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
 
-const autoPlay = () => {
-  const swipeNested = () => {
-    const nestedIndex = nestedSwiper.realIndex;
+const mainSwiper = new Swiper('.main-swiper', {
+  modules: [Pagination, Autoplay],
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 50,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
 
-    if (nestedIndex === nestedSlidesCount - 1) {
-      return mainSwiper.slideNext();
-    }
+mainSwiper.on('slideChange', () => {
+  if (mainSwiper.realIndex === NESTED_INDEX + 1) {
+    return nestedSwiper.slideTo(0);
+  }
 
-    return nestedSwiper.slideNext();
-  };
+  if (mainSwiper.realIndex === NESTED_INDEX) {
+    mainSwiper.autoplay.stop();
+    nestedSwiper.autoplay.start();
+  }
+});
 
-  const swipeMain = () => {
-    const mainIndex = mainSwiper.realIndex;
+nestedSwiper.on('slideChange', () => {
+  if (nestedSwiper.realIndex === nestedSlidesCount - 1) {
+    nestedSwiper.autoplay.stop();
+    mainSwiper.autoplay.start();
+  }
+});
 
-    if (mainIndex === NESTED_INDEX + 1) {
-      nestedSwiper.slideTo(0);
-    }
-
-    return mainSwiper.slideNext();
-  };
-
-  setInterval(() => {
-    const mainIndex = mainSwiper.realIndex;
-
-    if (mainIndex === NESTED_INDEX) {
-      return swipeNested();
-    }
-
-    return swipeMain();
-  }, AUTOPLAY_DELAY);
-};
-
-autoPlay();
+nestedSwiper.autoplay.stop();
