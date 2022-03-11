416545028d4a29c69058df2943d347db6f867e1b
allohamora <herman30698@gmail.com>
2022-03-11 11:04:38 +0200

    feat(slider-in-slider): add autoplay

--- topics/slider-in-slider/src/index.ts
+++ topics/slider-in-slider/src/index.ts
@@ -23,3 +23,43 @@ const nestedSwiper = new Swiper('.nested-swiper', {
     clickable: true,
   },
 });
+
+const nestedSlides = document.querySelectorAll('.nested-swiper .swiper-slide');
+const nestedSlidesCount = nestedSlides.length;
+
+const AUTOPLAY_DELAY = 1000;
+const NESTED_INDEX = 1;
+
+const autoPlay = () => {
+  const swipeNested = () => {
+    const nestedIndex = nestedSwiper.realIndex;
+
+    if (nestedIndex === nestedSlidesCount - 1) {
+      return mainSwiper.slideNext();
+    }
+
+    return nestedSwiper.slideNext();
+  };
+
+  const swipeMain = () => {
+    const mainIndex = mainSwiper.realIndex;
+
+    if (mainIndex === NESTED_INDEX + 1) {
+      nestedSwiper.slideTo(0);
+    }
+
+    return mainSwiper.slideNext();
+  };
+
+  setInterval(() => {
+    const mainIndex = mainSwiper.realIndex;
+
+    if (mainIndex === NESTED_INDEX) {
+      return swipeNested();
+    }
+
+    return swipeMain();
+  }, AUTOPLAY_DELAY);
+};
+
+autoPlay();
