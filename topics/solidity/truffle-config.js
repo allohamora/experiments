f2b5bcc43556cc73fd9f274addcb7def45c7e9ac
allohamora <herman30698@gmail.com>
2022-04-06 13:58:12 +0300

    feat(solidity): add and remove comments in config

--- topics/solidity/truffle-config.js
+++ topics/solidity/truffle-config.js
@@ -48,10 +48,11 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
+    // that network references to develop network for migration by one command without parameters: `truffle migrate`
     development: {
-      host: '127.0.0.1', // Localhost (default: none)
-      port: 8545, // Standard Ethereum port (default: none)
-      network_id: '*', // Any network (default: none)
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*',
     },
     // Another network with more advanced options...
     // advanced: {
