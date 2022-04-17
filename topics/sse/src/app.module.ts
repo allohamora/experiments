a8b0b34fda0bf67489218c9dfbb9537095c489c5
Allohamora <herman30698@gmail.com>
2022-04-17 15:25:43 +0300

    feat: add public serving

--- topics/sse/src/app.module.ts
+++ topics/sse/src/app.module.ts
@@ -1,9 +1,15 @@
+import path from 'node:path';
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.join(__dirname, '..', 'public'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
