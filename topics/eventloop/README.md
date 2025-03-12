1ccb1d3866eec1e4c18293c0a5bdaa7ee7358369
allohamora <herman30698@gmail.com>
2025-03-12 18:59:01 +0200

    feat(eventloop): extend eventloop topic

---
+++ topics/eventloop/README.md
@@ -0,0 +1,2 @@
+# Event Loop
+Event loops is a way to handle asynchronous operations in a program. It is a loop that checks one macro task and all micro tasks until there are no more tasks to execute.
--- topics/eventloop/micro-tasks.js
+++ topics/eventloop/micro-tasks.js
@@ -1,3 +1,5 @@
+import { createServer } from 'node:http';
+
 /**
  * @example
  * promise
@@ -48,19 +50,33 @@ const twoTimers = () => {
  * promise
  * process.nextTick
  * setImmediate
+ * process.nextTick in setImmediate
+ * promise in setImmediate
  * setTimeout
  * setTimeout 0
  */
 const logsMicroMacro = () => {
   console.log('before');
 
+  // the same level of priority
   queueMicrotask(() => console.log('queueMicrotask'));
   Promise.resolve().then(() => console.log('promise'));
+
   setTimeout(() => console.log('setTimeout'));
   setTimeout(() => console.log('setTimeout 0'), 0);
-  setImmediate(() => console.log('setImmediate'));
+  setImmediate(() => {
+    console.log('setImmediate');
 
+    Promise.resolve().then(() => console.log('promise in setImmediate'));
+
+    // but on the next iteration it will be before promise
+    process.nextTick(() => console.log('process.nextTick in setImmediate'));
+  });
+
+  // on the first iteration it will be after promise and queueMicrotask
   process.nextTick(() => console.log('process.nextTick'));
 
   console.log('after');
 };
+
+logsMicroMacro()
