bde73b36c4d0b33f6941363fa62d19fb691c9559
allohamora <herman30698@gmail.com>
2025-10-07 11:39:49 +0300

    feat(web-sst): add lang option

--- topics/web-tts/README.md
+++ topics/web-tts/README.md
@@ -1,5 +1,6 @@
 # web tts
-* SpeechRecognition does not have types out of the box
-* SpeechRecognition has a time limit after which it stops listening and you need to have a restart hack
-* SpeechRecognition has bad recognition results
-* speechSynthesis is really cool, it has all classic voices and some from Google
+
+- SpeechRecognition does not have types out of the box
+- SpeechRecognition has a time limit after which it stops listening and you need to have a restart hack
+- SpeechRecognition has bad recognition results without lang set
+- speechSynthesis is really cool, it has all classic voices and some from Google
--- topics/web-tts/src/services/speech.service.ts
+++ topics/web-tts/src/services/speech.service.ts
@@ -6,28 +6,39 @@ export class SpeechService {
 
   constructor() {
     this.recognition.continuous = true;
+    this.recognition.lang = 'en-US';
   }
 
   public startRecognition() {
     this.recognition.start();
 
-    this.recognition.addEventListener('end', (event) => {
-      console.log('end triggered', event);
-    
-      this.recognition.start();
-    }, { signal: this.abort.signal });
+    this.recognition.addEventListener(
+      'end',
+      (event) => {
+        console.log('end triggered', event);
+
+        this.recognition.start();
+      },
+      { signal: this.abort.signal },
+    );
   }
- 
+
   public stopRecognition() {
     this.abort.abort();
     this.recognition.stop();
   }
 
   public onRecognition(listener: (transcript: string) => void) {
-    this.recognition.addEventListener('result', (event) => {
-      const { transcript } = event.results.item(event.results.length - 1).item(0);
+    this.recognition.addEventListener(
+      'result',
+      (event) => {
+        const { transcript } = event.results.item(event.results.length - 1).item(0);
 
-      listener(transcript);
-    }, { signal: this.abort.signal });
+        console.log(event);
+
+        listener(transcript);
+      },
+      { signal: this.abort.signal },
+    );
   }
 }
