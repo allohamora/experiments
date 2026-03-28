efab4507241df28824366ec124d59dbb71be3f3a
allohamora <herman30698@gmail.com>
2026-03-28 12:39:53 +0200

    feat(kysely): add transferWithForUpdate

--- topics/kysely/src/index.ts
+++ topics/kysely/src/index.ts
@@ -1,4 +1,4 @@
-import { Kysely, PostgresDialect, sql, type Generated, type Insertable } from 'kysely';
+import { Kysely, PostgresDialect, sql, type Generated, type Insertable, type Selectable } from 'kysely';
 import { Pool } from 'pg';
 
 const { POSTGRES_URL } = process.env;
@@ -65,14 +65,55 @@ const getUsers = async () => {
   return await db.selectFrom('user').selectAll().execute();
 };
 
-const transfer = async (data: Insertable<Transfer>) => {
+const transferWithRaceCondition = async (data: Insertable<Transfer>) => {
   return await db.transaction().execute(async (trx) => {
     const from = await trx.selectFrom('user').selectAll().where('name', '=', data.from).executeTakeFirstOrThrow();
+    if (from.balance < data.amount) {
+      throw new Error('Insufficient balance');
+    }
+
     const to = await trx.selectFrom('user').selectAll().where('name', '=', data.to).executeTakeFirstOrThrow();
     await sql`select pg_sleep(1)`.execute(trx);
 
+    await trx
+      .updateTable('user')
+      .set({ balance: from.balance - data.amount })
+      .where('name', '=', from.name)
+      .execute();
+    await trx
+      .updateTable('user')
+      .set({ balance: to.balance + data.amount })
+      .where('name', '=', to.name)
+      .execute();
+
+    return await trx.insertInto('transfer').values(data).returningAll().executeTakeFirstOrThrow();
+  });
+};
+
+const transferWithForUpdate = async (data: Insertable<Transfer>) => {
+  return await db.transaction().execute(async (trx) => {
+    const targets = await trx
+      .selectFrom('user')
+      .selectAll()
+      .where('name', 'in', [data.from, data.to])
+      .forUpdate()
+      .execute();
+    if (targets.length !== 2) {
+      throw new Error('Invalid users');
+    }
+
+    const from = targets.find((user) => user.name === data.from);
+    if (!from) {
+      throw new Error('Invalid from user');
+    }
+
     if (from.balance < data.amount) {
       throw new Error('Insufficient balance');
+    }
+
+    const to = targets.find((user) => user.name === data.to);
+    if (!to) {
+      throw new Error('Invalid to user');
     }
 
     await trx
@@ -94,7 +135,7 @@ const getTransfers = async () => {
   return await db.selectFrom('transfer').selectAll().execute();
 };
 
-const raceCondition = async () => {
+const raceCondition = async (transfer: (data: Insertable<Transfer>) => Promise<Selectable<Transfer>>) => {
   const alice = await createUser({ name: 'alice', balance: 1000 });
   const bob = await createUser({ name: 'bob', balance: 1000 });
   const charlie = await createUser({ name: 'charlie', balance: 1000 });
@@ -102,6 +143,9 @@ const raceCondition = async () => {
   await Promise.all([
     transfer({ from: alice.name, to: bob.name, amount: 100 }),
     transfer({ from: alice.name, to: charlie.name, amount: 100 }),
+    // deadlock detected in transferWithRaceCondition
+    // transfer({ from: bob.name, to: alice.name, amount: 100 }),
+    // transfer({ from: charlie.name, to: alice.name, amount: 100 }),
   ]);
 
   const users = await getUsers();
@@ -112,5 +156,6 @@ const raceCondition = async () => {
 };
 
 await createTables();
-await raceCondition().catch(console.error);
+await raceCondition(transferWithForUpdate);
 await dropTables();
+await db.destroy();
