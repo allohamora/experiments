e0623d117b42e7b287a3ff7406f5543feb6b6daa
allohamora <herman30698@gmail.com>
2026-05-22 08:59:57 +0300

    feat(tanstack-start): add nitro nodejs build output

--- topics/tanstack-start/vite.config.ts
+++ topics/tanstack-start/vite.config.ts
@@ -5,10 +5,11 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 
 import viteReact from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { nitro } from 'nitro/vite';
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [devtools(), tailwindcss(), tanstackStart(), nitro(), viteReact()],
 });
 
 export default config;
