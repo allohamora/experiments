f9315dba264c35875864f7a5c284c342cb44b4fe
allohamora <herman30698@gmail.com>
2026-05-27 18:33:34 +0300

    feat(effect): add di example

--- topics/effect/src/index.ts
+++ topics/effect/src/index.ts
@@ -1,5 +1,14 @@
 import { Effect, Console } from 'effect';
 
-const program = Console.log('Hello, Effect Topic!');
+class Logger extends Effect.Tag('Logger')<Logger, { log: (message: string) => Effect.Effect<void, never, never> }>() {}
+
+const ConsoleLogger = Logger.of({
+  log: (message: string) => Console.log(message),
+});
+
+const program = Effect.gen(function* () {
+  const logger = yield* Logger;
+  yield* logger.log('Hello, Effect!');
+}).pipe(Effect.provideService(Logger, ConsoleLogger));
 
 Effect.runSync(program);
